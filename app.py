2d906d3250406a71fa31923a1c06016e638801ce
Rakesh-droid <49478556+Rakesh-droid@users.noreply.github.com>
2020-06-29 16:51:22 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request,jsonify,render_template
+import pickle
+
+app = Flask(__name__)
+filename = 'model.pkl'
+model = pickle.load(open(filename, 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods = ['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text='Price of the mobile be $ {}'.format(output))
+    
+if __name__ == "__main__":
+    app.run(debug =True)
